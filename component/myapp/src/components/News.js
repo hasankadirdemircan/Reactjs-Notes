9dbb924b2ea60f00361d057fc715694aef20d23e
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2019-10-02 22:00:08 +0300

    List and ListItem component example added.

---
+++ component/myapp/src/components/News.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import NewsItem from './NewsItem';
+
+class News extends Component {
+    
+    render(){
+        return(
+            <div>
+                   <NewsItem/>
+                   <NewsItem/>
+                   <NewsItem/>
+            </div>
+        )
+    }
+}
+
+export default News;
