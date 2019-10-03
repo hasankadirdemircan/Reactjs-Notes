43c2c1ce82a0a1de45ac43bc308dba98e71d0c04
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2019-10-03 20:17:30 +0300

    react js expression example

--- component/myapp/src/components/News.js
+++ component/myapp/src/components/News.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import NewsItem from './NewsItem';
 
 class News extends Component {
-    
+   //js expression yazabilirsin. 
     render(){
+        let nameElement = <div>demircan</div>
         return(
             <div>
+                {2 + 2 === 4 ? "evet" : "hayır"} 
+                {nameElement}
                    <NewsItem/>
                    <NewsItem/>
                    <NewsItem/>
