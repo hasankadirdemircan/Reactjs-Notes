7549820c8a8fa95d060c5b5e12a670aff3c6253c
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2019-10-03 20:14:40 +0300

    className

--- component/myapp/src/components/NewsItem.js
+++ component/myapp/src/components/NewsItem.js
@@ -1,15 +1,17 @@
 import React , {Component} from 'react';
-
+import '../NewsItem.css'
 //component
 class NewsItem extends Component{
     render(){
         return (
             //tek div icerisinde olmali hersey
             <div>
-                <h1>Hello React</h1>
+                <h1 className="title">Hello React</h1>
                 <p>
                     Deneme yazısı
                 </p>
+
+                <label htmlFor=""></label>
             </div>
         )
     }
