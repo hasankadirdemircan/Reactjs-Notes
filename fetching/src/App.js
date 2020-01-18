41b05308d0a2724dd180df344b82400618cf6e3e
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2020-01-18 13:23:30 +0300

    native fetching added.

---
+++ fetching/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: []
+  };
+  //api cagirimlari icin en uygun yapi.
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(data => data.json())
+    .then(users => {
+      this.setState({
+        users,
+      })
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {
+          this.state.users.map(user => 
+            <div key={user.id} className={"userList"}>
+              {user.name} - @{ user.username }
+            </div>
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
